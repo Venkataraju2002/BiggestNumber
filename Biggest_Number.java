1330c6f8c9c0a092afae86934a229611d4260561
Venkataraju2002 <84319124+Venkataraju2002@users.noreply.github.com>
2021-08-14 12:00:10 +0530

    Add files via upload

---
+++ Biggest_Number.java
@@ -0,0 +1,18 @@
+import java .util.*;
+class Biggest_Number
+{
+public static void main(String args[])
+{
+int a,b;
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the numbers");
+a  = sc.nextInt();
+System .out.println("enter the value a");
+b =sc.nextInt();
+System .out.println("enter the value b");
+if(a>b)
+{System.out.print(a+"bigger then"+b);}
+else
+{System.out.println(b+"biggerthen"+a);}
+}}
